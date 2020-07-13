845d7437fdf88a60ba9ca5345c10139998360061
SUMing <liboysu@163.com>
2020-07-13 23:59:59 +0800

    feat: init

---
+++ react-app-sub1/src/routes.ts
@@ -0,0 +1,19 @@
+import Home from './pages/Home';
+import Test from './pages/Test';
+
+const routes = [
+	{
+		link: '/',
+		component: Home,
+	},
+	{
+		link: '/home',
+		component: Home,
+	},
+	{
+		link: '/test',
+		component: Test,
+	},
+];
+
+export default routes;
